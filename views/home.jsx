43a804f2997ff30dd911a6642aa90d828293679a
deankp <dperez0689@gmail.com>
2022-06-09 19:44:26 -0500

    updated jsx

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img class="fruityDrink" src="/images/fruity-drink.jpg" alt="Fruity Drink" />
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
