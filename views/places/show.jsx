f576e4e031686d5d0e52982f8bfb3363cf558594
deankp <dperez0689@gmail.com>
2022-06-23 23:59:49 -0500

    added comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
@@ -31,7 +50,7 @@ function show (data) {
               <div className="row justify-content-center mt-5 border-top">
                 <div className="col pt-3">
                 <h2>Comments</h2>
-                <p>No Comments Yet!</p>
+                {comments}
                 </div>
               </div>
 
