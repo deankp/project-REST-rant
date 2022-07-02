15e1a42e1bd4cc1cbe67d7f58a3be99213dfed5f
deankp <dperez0689@gmail.com>
2022-07-01 22:39:08 -0500

    updated formatting

--- views/default.jsx
+++ views/default.jsx
@@ -4,7 +4,7 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>RestRant Live Demo</title>
+                <title>RestRant</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
