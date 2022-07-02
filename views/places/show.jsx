7e6c370b11d5157032e105367536c8ab3fb841ed
deankp <dperez0689@gmail.com>
2022-07-01 22:23:21 -0500

    updated formatting

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,7 +3,9 @@ const Def = require('../default')
 
 function show (data) {
   let comments = (
-    <h3 className="inactive">No comments yet!</h3>
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
   )
   let rating = (
     <h3 className="inactive">
@@ -26,85 +28,90 @@ function show (data) {
     )
     comments = data.place.comments.map(c => {
       return (
-        <div className="border">
-          <h2 className='rant'>{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
+        <div className="border col-sm-4">
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
           <h4>{c.content}</h4>
           <h3>
-            <strong>- {c.author}</strong>
+            <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
+          <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+            <input type="submit" className="btn btn-danger" value="Delete Comment" />
+          </form>
         </div>
       )
     })
   }
-    return (
-        <Def>
-          <main>
-            <div className="container mt-5">
-              <div className="row align-items-center">
-                <div className="col">
-                <img src={ data.place.pic } alt={ data.place.name } />
-                <h3>Located in { data.place.city }, { data.place.state }</h3>
-                </div>
-                <div className="col">
-                  <h1>{ data.place.name }</h1>
-                  <h2>Rating</h2>
-                  {rating}
-                  <br />
-                  <p>Not Rated</p>
-
-                  <h2>Description</h2>
-                  <h3>{data.place.showEstablished()}</h3>
-                  <h4>{data.place.cuisines}</h4>
-                </div>
-              </div>
-              <div className="row justify-content-center">
-                <div className="col text-right">
-                  <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
-                  Edit
-                  </a>
-                  
-                </div>
-                <div className="col text-left">
-                  <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">DELETE</button>
-                  </form></div>
-              </div>
-
-              <div className="row justify-content-center mt-5 border-top">
-                <div className="col pt-3">
-                <h2>Comments</h2>
-                {comments}
-
-                <form method='POST' action={`/places/${data.place.id}/comment`}>
-                  <div className='form-group'>
-                    <label htmlFor='author'>Author</label>
-                    <input className='form-control' id='author' name='author'/>
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='content'>Content</label>
-                    <input className='form-control' id='content' name='content' type='textarea' />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='stars'>Star Rating</label>
-                    <input className='form-control' id='stars' name='stars' type='range' step='0.5' min='0' max='5' />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='rant'>Rant</label>
-                    <input  id='rant' name='rant' type='checkbox' defaultChecked/>
-                  </div>
-
-                  <input className='btn btn-primary' type='submit' value='Add Comment' />
-                </form>
-                </div>
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={data.place.pic} alt={data.place.name} />
+              <h3>
+                Located in {data.place.city}, {data.place.state}
+              </h3>
+            </div>
+            <div className="col-sm-6">
+              <h1>{ data.place.name }</h1>
+              <h2>
+                Rating
+              </h2>
+              {rating}
+              <br />
+              <h2>
+                Description
+              </h2>
+              <h3>
+                {data.place.showEstablished()}
+              </h3>
+              <h4>
+                Serving {data.place.cuisines}
+              </h4>
+              <br />
+              <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                Edit
+              </a>{` `}
+              <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+              </form>
+            </div>
+          </div>
+          <hr />
+          <h2>Comments</h2>
+          <div className="row">
+            {comments}
+          </div>
+          <hr />
+          <h2>Got Your Own Rant or Rave?</h2>
+          <form action={`/places/${data.place.id}/comment`} method="POST">
+            <div className="row">
+              <div className="form-group col-sm-12">
+                <label htmlFor="content">Content</label>
+                <textarea id="content" name="content" className="form-control"></textarea>
               </div>
             </div>
-          </main>
-        </Def>
-    )
+            <div className="row">
+              <div className="form-group col-sm-4">
+                <label htmlFor="author">Author</label>
+                <input id="author" name="author" className="form-control" />
+              </div>
+              <div className="form-group col-sm-4">
+                <label htmlFor="stars">Star Rating</label>
+                <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+              </div>
+              <div className="form-group col-sm-2">
+                <label htmlFor="rant">Rant?</label>
+                <input type="checkbox" id="rant" name="rant" className="form-control" />
+              </div>
+            </div>
+            <input type="submit" className="btn btn-primary" value="Add Comment" />
+          </form>
+        </main>
+      </Def>
+  )
 }
 
 module.exports = show
